7b719bfb68939b01e4230856f8842d85c714d8a1
dsk79 <37499808+dsk79@users.noreply.github.com>
2018-03-18 03:55:21 -0700

    Test
    Test

--- TicTacToe.py
+++
@@ -1,6 +0,0 @@
-class Board:
-
-    def __init__(self, rows, cols):
-        self.rows = rows
-        self.cols = cols
-
---
+++ main.py
@@ -0,0 +1,6 @@
+import Board
+
+print('Hello first')
+
+game = Board(3, 3)
+#game.create_board()
